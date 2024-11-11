1c357c539962ca46c6ede5bdd3809f7ffc444e5c
Aisulu <aisulutulepova4@gmail.com>
2024-11-11 10:55:11 +0500

    first commit

---
+++ baqylay.js
@@ -0,0 +1,35 @@
+/*
+//Тапсырма A: Ай атауын шығару
+let san = +prompt('1-ден 12-ге дейін сан енгізіңіз:')
+
+
+if (san == 1 || san == 12 || san == 2  ){
+    alert("Маусым : Winter" + san)
+}
+if (san == 3 || san == 4 || san==5 ){
+    alert('Маусым : Spring' + san)
+}
+if (san == 6 || san == 7 || san ==8 ){
+    alert('Маусым: Summer' + san )
+}
+if (san == 9 || san == 10 || san ==11 ){
+    alert('Маусым: Autumn' + san )
+}
+else{
+    alert('дұрыс емес сан енгіздіңіз')
+}
+  */  
+
+
+
+// Тапсырма B: Екі санды салыстырып, орташа мәнді табу 
+let numa = +prompt(" a санын енгізіңіз")
+let numb = +prompt('b санын енгізіңіз')
+
+    switch (numa == (a>b)){
+       case numa 
+       alert('a саны  b  санынан үлкен' + numa)
+       break
+    }
+   
+
